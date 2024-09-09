cfd82a234f23b4ef89e1cbf350cebe3173a2a901
marktipton <marktipton95@gmail.com>
2024-09-09 13:43:39 -0500

    fetching data in music player insteazd of playlist. Data is being fetched successfully

--- src/MusicPlayer.tsx
+++ src/MusicPlayer.tsx
@@ -1,11 +1,39 @@
-import CurrentlyPlaying from "./components/CurrentlyPlaying";
-import Playlist from "./components/Playlist";
+import React, { useEffect, useState } from 'react';
+import CurrentlyPlaying from './components/CurrentlyPlaying';
+import Playlist from './components/Playlist';
+
+type PlaylistItem = {
+  id: number;
+  title: string;
+  artist: string;
+  genre: string;
+  duration: string;
+  cover: string;
+};
 
 export default function MusicPlayer() {
+  const [playlist, setPlaylist] = useState<PlaylistItem[]>([]);
+  const [currentSong, setCurrentSong] = useState<PlaylistItem | null>(null);
+
+  useEffect(() => {
+    const fetchPlaylistData = async () => {
+      const response = await fetch('https://raw.githubusercontent.com/atlas-jswank/atlas-music-player-api/main/playlist');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data: PlaylistItem[] = await response.json();
+      setPlaylist(data);
+    };
+
+    fetchPlaylistData();
+  }, []);
+
+  console.log('Playlist state:', playlist); // Log the playlist state
+
   return (
   <div className="flex flex-col md:flex-row shadow-lg rounded-lg divide-x divide-y">
-    <CurrentlyPlaying/>
-    <Playlist/>
+    <CurrentlyPlaying currentSong={currentSong} />
+    <Playlist playlist={playlist} />
   </div>
   );
 }
--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PlayListItem from './PlayListItem'
 
 type PlaylistItem = {
@@ -10,22 +10,11 @@ type PlaylistItem = {
   cover: string;
 };
 
-const Playlist = () => {
-  const [playlist, setPlaylist] = useState<PlaylistItem[]>([]);
+type PlaylistProps = {
+  playlist: PlaylistItem[];
+};
 
-  useEffect(() => {
-    const fetchPlaylistData = async () => {
-      const response = await fetch('https://raw.githubusercontent.com/atlas-jswank/atlas-music-player-api/main/playlist');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data: PlaylistItem[] = await response.json();
-      setPlaylist(data);
-    };
-
-    fetchPlaylistData();
-  }, []);
-
+function Playlist({playlist}: PlaylistProps) {
   return (
     <div className='p-6 md:w-1/2'>
       <h1 className='font-medium text-xl pb-4'>Playlist</h1>
