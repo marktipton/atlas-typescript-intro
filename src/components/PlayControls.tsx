1ac7a15fea21b282fd8698145677d0b880842d85
marktipton <marktipton95@gmail.com>
2024-09-09 12:06:20 -0500

    added state for speed button. functioning correctly

--- src/components/PlayControls.tsx
+++ src/components/PlayControls.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const PlayControls = () => {
+  const [speed, setSpeed] = useState<'1x' | '2x' | '3x'>('1x');
+
+  const speedClick = () => {
+      setSpeed((prevSpeed) => {
+        switch (prevSpeed) {
+          case '1x':
+            return '2x';
+          case '2x':
+            return '3x';
+          case '3x':
+            return '1x';
+        }
+      });
+    };
+
   return (
     <div className='flex justify-between items-center h-8'>
-      <button>1x</button>
+      <button onClick={speedClick}>{speed}</button>
       <button>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-secondary">
           <path d="M9.195 18.44c1.25.714 2.805-.189 2.805-1.629v-2.34l6.945 3.968c1.25.715 2.805-.188 2.805-1.628V8.69c0-1.44-1.555-2.343-2.805-1.628L12 11.029v-2.34c0-1.44-1.555-2.343-2.805-1.628l-7.108 4.061c-1.26.72-1.26 2.536 0 3.256l7.108 4.061Z" />
