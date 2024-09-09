e0eb7955cff2517c3e6b5c16144b69c8dcb0d3a2
marktipton <marktipton95@gmail.com>
2024-09-09 13:05:35 -0500

    succesfully fetched data from api

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PlayListItem from './PlayListItem'
 
+type PlaylistItem = {
+  id: number;
+  title: string;
+  artist: string;
+  genre: string;
+  duration: string;
+  cover: string;
+};
+
 const Playlist = () => {
+  const [playlist, setPlaylist] = useState<PlaylistItem[]>([]);
+
+  useEffect(() => {
+    const fetchPlaylistData = async () => {
+      const response = await fetch('https://raw.githubusercontent.com/atlas-jswank/atlas-music-player-api/main/playlist');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data: PlaylistItem[] = await response.json();
+      setPlaylist(data);
+    };
+
+    fetchPlaylistData();
+  }, []);
+
   return (
     <div className='p-6 md:w-1/2'>
       <h1 className='font-medium text-xl pb-4'>Playlist</h1>
-      <PlayListItem name='Painted in Blue' artist='Soul Canvas' time='3:21' bgColor="bg-secondary-light"/>
-      <PlayListItem name='Tidal Drift' artist='Echoes of the Sea' time='8:02'/>
-      <PlayListItem name='Fading Shadows' artist='The Emberlight' time='3:01'/>
-      <PlayListItem name='Cosmic Drift' artist='Solar Flare' time='5:01'/>
-      <PlayListItem name='Urban Serenade' artist='Midnight Groove' time='4:54'/>
-      <PlayListItem name='Whispers in the Wind' artist='Rust & Ruin' time='6:13'/>
-      <PlayListItem name='Electric Fever' artist='Neon Jungle' time='8:41'/>
-      <PlayListItem name='Edge of the Abyss' artist='Steel Horizon' time='2:27'/>
-      <PlayListItem name='Golden Haze' artist='Velvet Waves' time='3:15'/>
-      <PlayListItem name='Shatter the Silence' artist='Thunderclap Echo' time='8:22'/>
+      {playlist.map(item => (
+        <PlayListItem
+          key={item.id}
+          name={item.title}
+          artist={item.artist}
+          time={item.duration}
+          bgColor="bg-secondary-light"  // or another color based on your needs
+        />
+      ))}
     </div>
   )
 }
