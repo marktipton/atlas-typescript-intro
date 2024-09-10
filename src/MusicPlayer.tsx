f3345bf2f983bb771b1c22983b567a7b7a3a1419
marktipton <marktipton95@gmail.com>
2024-09-10 16:05:20 -0500

    shuffle working for next arrow. and working with clicked songs

--- src/MusicPlayer.tsx
+++ src/MusicPlayer.tsx
@@ -16,6 +16,7 @@ export default function MusicPlayer() {
   const [currentSong, setCurrentSong] = useState<PlaylistItem | null>(null);
   const [currentSongIndex, setCurrentSongIndex] = useState<number>(0);
   const [isShuffled, setIsShuffled] = useState<boolean>(false);
+  const [playedSongs, setPlayedSongs] = useState<number[]>([]); // keep track of played songs for shuffle mode
 
   useEffect(() => {
     const fetchPlaylistData = async () => {
@@ -28,19 +29,26 @@ export default function MusicPlayer() {
       if (data.length > 0) {
         setCurrentSong(data[0]);
         setCurrentSongIndex(0);
+        setPlayedSongs([0]);
       }
     };
 
     fetchPlaylistData();
   }, []);
 
+
   const handleNextSong = () => {
-    if (currentSongIndex < playlist.length - 1) {
-      const nextIndex = currentSongIndex + 1;
-      setCurrentSong(playlist[nextIndex]);
-      setCurrentSongIndex(nextIndex);
+    if (isShuffled) {
+      handleShuffleNext();
+    } else {
+      if (currentSongIndex < playlist.length - 1) {
+        const nextIndex = currentSongIndex + 1;
+        setCurrentSong(playlist[nextIndex]);
+        setCurrentSongIndex(nextIndex);
+      }
     }
   };
+
 
   const handlePrevSong = () => {
     if (currentSongIndex > 0) {
@@ -50,13 +58,36 @@ export default function MusicPlayer() {
     }
   };
 
+  const handleShuffleNext = () => {
+    let newPlayedSongs = [...playedSongs];
+
+    if (newPlayedSongs.length === playlist.length) {
+      // All songs have been played, reset the played list after selecting a new song
+      newPlayedSongs = [];
+    }
+
+    const remainingSongs = playlist
+      .map((_, index) => index)  // Get all indices
+      .filter(index => !newPlayedSongs.includes(index)); // Filter out already played songs
+
+    const randomIndex = remainingSongs[Math.floor(Math.random() * remainingSongs.length)];
+    newPlayedSongs.push(randomIndex); // Add the new song to played list
+
+    setPlayedSongs(newPlayedSongs); // Update played songs state
+    setCurrentSong(playlist[randomIndex]);
+    setCurrentSongIndex(randomIndex);
+  };
+
   const handleShuffleToggle = (isShuffled: boolean) => {
     setIsShuffled(isShuffled);
   };
 
   const handleSongClick = (song: PlaylistItem, index: number) => {
     setCurrentSong(song);
-    setCurrentSongIndex(index);  // Update current song and index
+    setCurrentSongIndex(index);
+    if (isShuffled && !playedSongs.includes(index)) {
+      setPlayedSongs([...playedSongs, index]); // Add clicked song to played list only in shuffle mode
+    }
   };
 
   const isPrevDisabled = currentSongIndex === 0 && !isShuffled;
